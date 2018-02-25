294f6a31e81114d50967e7a68833d0c42045b1ba
Matheus Melo Antiquera <matheus.melo.a@hotmail.com>
2018-02-25 16:55:36 -0300

    Cadastro de Cliente e Endereço

--- app/ClientModel.php
+++ app/ClientModel.php
@@ -3,8 +3,51 @@
 namespace api_cliente;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class ClientModel extends Model
 {
-    //
+    protected $fillable = ['nome', 'telefone', 'celular', 'cliente', 'cidade',
+      'logradouro','numero','complemento'];
+
+    protected $guarded = ['id', 'created_at', 'update_at'];
+
+    protected $dates = ['deleted_at'];
+
+    public $timestamps = true;
+
+    public static function createClient($data){
+        
+        DB::beginTransaction();
+        
+        try{
+
+            //Insere o Cliente
+            $idClient = DB::table('cliente')->insertGetId([
+                'nome' => $data['name'],
+                'telefone' => $data['fone'],
+                'celular' => $data['cellphone'],
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ]);
+           
+            DB::table('endereco')->insert([
+                'cliente' => $idClient,
+                'cidade' => $data['city'],
+                'logradouro' => $data['address'],
+                'numero' => $data['number'],
+                'complemento' => $data['complement'],
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ]);
+            
+            
+            DB::commit();
+                        
+            return "ok";
+        } 
+        catch(\Exception $e){
+            DB::rollback();
+            return $e;
+        }
+    }
 }
--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -1,10 +1,25 @@
 <?php
 
 namespace api_cliente\Http\Controllers;
-
+use api_cliente\ClientModel;
 use Illuminate\Http\Request;
+
 
 class ClientController extends Controller
 {
-    //
+    public function create(Request $request){
+        
+        $data = [
+            'name' => $request->input('name'),
+            'fone' => $request->input('fone'),
+            'cellphone' => $request->input('cellphone'),
+            'city' => $request->input('city'),
+            'address' => $request->input('address', NULL),
+            'number' => $request->input('number', NULL),
+            'complement' => $request->input('complement',NULL ),
+                                               
+        ];
+        
+       return ClientModel::createClient($data);
+    }
 }
